638032bbf619c6c272e1c2fc18f4186697124a9e
Karl Tatom <karltatom@live.com>
2017-09-11 22:31:14 -0700

    Initial commit

---
+++ app/eventPage.js
@@ -0,0 +1,106 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create(getMenuItem("launch_practice", "Launch Practice", "normal"));
+    chrome.contextMenus.create(getMenuItem("sep1", "Separator", "separator"));
+
+    const ids = ["flip_tree", "hide_gold", "hide_locked"],
+        titles = ["Flip Tree", "Hide Gold Skills", "Hide Locked Skills"];
+
+    for (let i = 0, l = ids.length; i < l; i++) {
+        chrome.contextMenus.create(getMenuItem(ids[i], titles[i], "checkbox"));
+    }
+});
+
+function getMenuItem(id, title, type) {
+    return {
+        "contexts": ["all"],
+        "documentUrlPatterns": ["*://*.duolingo.com/*"],
+        "id": id,
+        "title": title,
+        "type": type
+    };
+}
+
+chrome.runtime.onStartup.addListener(function() {
+    chrome.storage.sync.get(["flip_tree", "hide_gold", "hide_locked"],
+        function(settings) {
+            for (let setting in settings) {
+                if (!settings.hasOwnProperty(setting)) continue;
+
+                chrome.contextMenus.update(setting, { "checked": settings[setting] });
+            }
+        });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+    switch (info.menuItemId) {
+        case "launch_practice":
+            launchPractice();
+            break;
+        case "sep1":
+            break;
+        default:
+            setSetting(info.menuItemId, info.checked);
+    }
+});
+
+chrome.storage.onChanged.addListener(function(setting) {
+    const settingName = Object.keys(setting)[0],
+        newValue = setting[settingName]["newValue"],
+        settingObject = {
+            "setting": settingName,
+            "value": newValue
+        };
+
+    chrome.contextMenus.update(settingName, { "checked": newValue });
+
+    console.log("Settings", settingObject);
+
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        if (!tabs.length) return;
+
+        chrome.tabs.sendMessage(tabs[0].id, settingObject);
+    });
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+    console.log("Command:", command);
+
+    if (command.indexOf("toggle_") !== -1) {
+        toggleSetting(command.replace("toggle_", ""));
+    } else if (command === "launch_practice") {
+        launchPractice();
+    } else {
+        console.log("Unknown command. How did this even happen?");
+    }
+});
+
+function toggleSetting(setting) {
+    chrome.storage.sync.get(setting, function(settings) {
+        setSetting(setting, !settings[setting]);
+    });
+}
+
+function setSetting(setting, value) {
+    chrome.storage.sync.set({ [setting]: value });
+}
+
+function launchPractice() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const url = { "url": "https://www.duolingo.com/practice" }
+
+        if (tabs[0].url.indexOf("duolingo") === -1) {
+            chrome.tabs.query(url, function(tabs2) {
+                if (tabs2.length) {
+                    chrome.tabs.update(tabs2[0].id, { "active": true });
+                } else {
+                    chrome.tabs.create(url);
+                }
+            });
+        } else {
+            chrome.tabs.update(tabs[0].id, url);
+        }
+    });
+}
---
+++ app/flip_tree_hide_gold/content.js
@@ -0,0 +1,130 @@
+"use strict";
+
+var root, observer, buttonsDiv, observerInterval;
+
+const retryInterval = window.setInterval(initialize, 1000);
+
+// Functions
+
+function initialize() {
+    const storeButton = getStoreButton();
+    if (!storeButton) return;
+    window.clearInterval(retryInterval);
+
+    root = document.getElementById("root");
+    observer = new MutationObserver(refreshTree);
+
+    chrome.runtime.onMessage.addListener(refreshTree);
+
+    initializeButtons(storeButton);
+    refreshTree();
+}
+
+function initializeButtons(storeButton) {
+    buttonsDiv = document.createElement("div");
+    buttonsDiv.id = "cl-tree-mods";
+
+    const hideGoldButton = storeButton.cloneNode();
+    hideGoldButton.id = "hide_gold";
+    hideGoldButton.removeAttribute("data-test");
+    hideGoldButton.setAttribute("href", "javascript:void(0);");
+
+    const titleSpan = document.createElement("span"),
+        iconSpan = document.createElement("span"),
+        flipTreeButton = hideGoldButton.cloneNode(),
+        hideLockedButton = hideGoldButton.cloneNode();
+
+    titleSpan.innerText = "Flip Tree";
+    iconSpan.id = "cl-icon-arrow";
+    flipTreeButton.id = "flip_tree";
+    hideLockedButton.id = "hide_locked";
+
+    flipTreeButton.appendChild(iconSpan);
+    flipTreeButton.appendChild(titleSpan);
+
+    [flipTreeButton, hideGoldButton, hideLockedButton].forEach(wireButton);
+
+    // Functions
+
+    function wireButton(button) {
+        button.addEventListener("click", handleButtonClick);
+        buttonsDiv.appendChild(button);
+    }
+
+    function handleButtonClick() {
+        const id = this.id,
+            toggle = !root.classList.contains(id);
+
+        chrome.storage.sync.set({ [id]: toggle });
+    }
+}
+
+function refreshTree() { // Called frequently; do not nest functions
+    const storeButton = getStoreButton();
+    if (!storeButton) return;
+
+    stopObserver();
+    classifyAllSkillParents();
+
+    if (!storeButton.getAttribute("data-hooked")) {
+        storeButton.parentElement.appendChild(buttonsDiv);
+        storeButton.setAttribute("data-hooked", true);
+    }
+
+    matchRootClassesToAllSettings();
+    startObserver();
+}
+
+function getStoreButton() {
+    return document.querySelector("[data-test=lingot-store-button]");
+}
+
+function classifyAllSkillParents() {
+    Array.from(document.querySelector("[data-test=skill-tree]").children).forEach(classifyParent);
+}
+
+function classifyParent(parentGroup) {
+    parentGroup.classList.toggle("gold-only", parentGroup.querySelectorAll("[data-test~=gold]").length === parentGroup.children.length);
+    parentGroup.classList.toggle("locked", !parentGroup.querySelector("a") || !!parentGroup.querySelector("[data-test=test-out-button]"));
+}
+
+function matchRootClassesToAllSettings() {
+    chrome.storage.sync.get(["hide_locked", "hide_gold", "flip_tree"], gotSettingValues);
+}
+
+function gotSettingValues(settings) {
+    for (let setting in settings) {
+        if (!settings.hasOwnProperty(setting)) continue;
+
+        matchRootClassToSetting(setting, settings[setting]);
+    }
+}
+
+function matchRootClassToSetting(className, val) {
+    root.classList.toggle(className, val);
+    console.log("Update class", className, val);
+}
+
+function startObserver() {
+    if (!observer) return;
+
+    window.clearTimeout(observerInterval);
+
+    observerInterval = window.setTimeout(observe, 500);
+}
+
+function observe() {
+    observer.observe(root,
+        {
+            "subtree": true,
+            "childList": true,
+            "attributes": true,
+            "characterData": true
+        });
+}
+
+function stopObserver() {
+    if (!observer) return;
+
+    observer.disconnect();
+}
